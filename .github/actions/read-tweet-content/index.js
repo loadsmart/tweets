04631a0359ce8f79318e413e4eb01f4366954a24
Gustavo Barbosa <gustavocsb@gmail.com>
2020-04-22 09:57:14 -0400

    include github token;

--- .github/actions/read-tweet-content/index.js
+++ .github/actions/read-tweet-content/index.js
@@ -8,7 +8,8 @@ async function run() {
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
     const pull_number = github.context.payload.pull_request.number;
 
-    const { files } = await octokit.request(
+    octokit.request();
+    const { data } = await octokit.request(
       "GET /repos/:owner/:repo/pulls/:pull_number/files",
       {
         owner,
@@ -17,14 +18,13 @@ async function run() {
       }
     );
 
-    console.log("files", files);
+    console.log(owner, repo, pull_number);
+    console.log("files", data);
     core.setFailed("on purpose");
-    return;
 
-    // TODO read from file in the PR
-    const content = "This is supposed to be the content of the tweet";
+    // const content = "This is supposed to be the content of the tweet";
 
-    core.setOutput("content", content);
+    // core.setOutput("content", content);
   } catch (error) {
     core.setFailed(error.message);
   }
